78a20242392e025a3cb47b7ea93eb59723fc32c9
D <me@kumo.dev>
2021-05-27 23:17:38 +1000

    add connection details to dotenv

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,6 +1,7 @@
 import pg from 'pg';
 
-const pool = new pg.Pool({ connectionString: 'postgresql://localhost/paradb' });
+// Connection details are pulled from env variables: https://node-postgres.com/features/connecting
+const pool = new pg.Pool();
 pool.on('error', err => console.error(err));
 
 export default pool;
