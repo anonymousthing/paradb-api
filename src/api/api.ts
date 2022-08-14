e9d008d367858a10d3e0b8f23c81585c064c3bdd
D <me@kumo.dev>
2022-08-15 01:47:11 +1000

    infra: set MIME type to application/x-protobuf for API responses

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,6 +4,14 @@ import usersRouter from 'services/users/users';
 
 export function createApiRouter(mapsDir: string) {
   const apiRouter = express.Router({ strict: true });
+  apiRouter.use((req, res, next) => {
+    // Set MIME type for responses to 'application/x-protobuf' to allow Cloudflare to use
+    // brotli / gzip encoding over the wire. We don't actually use protobuf but Cloudflare doesn't
+    // let you force compression for 'application/octet-stream', so this is the closest.
+    res.contentType('application/x-protobuf');
+    next();
+  });
+
   // Logged out routes
   const mapsRouter = createMapsRouter(mapsDir);
   apiRouter.use('/maps', mapsRouter);
