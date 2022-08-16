a248cba25741b83e9d048ffe9bc0f4e4b0415673
D <me@kumo.dev>
2022-08-16 18:21:19 +1000

    tests: run db commands as postgres user

--- tools/test.sh
+++ tools/test.sh
@@ -2,11 +2,12 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TEST_DB="paradb_test"
+POSTGRES_USER="postgres"
 
 echo "Dropping database: $TEST_DB"
-dropdb "$TEST_DB"
-createdb "$TEST_DB"
-psql -d "$TEST_DB" -f "$SCRIPT_DIR/../db/init.sql"
+su -c "dropdb \"$TEST_DB\"" "$POSTGRES_USER"
+su -c "createdb \"$TEST_DB\"" "$POSTGRES_USER"
+su -c "psql -d \"$TEST_DB\" -f \"$SCRIPT_DIR/../db/init.sql\"" "$POSTGRES_USER"
 
 echo "Running tests..."
 yarn jest
