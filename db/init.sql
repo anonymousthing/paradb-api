3d5fc1e4952e091bd9004134f9e8b67d253be382
D <me@kumo.dev>
2022-08-22 12:15:43 +1000

    favorites: add table to init.sql and create migration script

--- db/init.sql
+++ db/init.sql
@@ -28,3 +28,12 @@ CREATE TABLE users (
 );
 CREATE INDEX idx_users_username ON users (lower(username));
 CREATE INDEX idx_users_email ON users (lower(email));
+
+CREATE TABLE favorites (
+  map_id varchar(16) references maps (id) not null,
+  user_id varchar(16) references users (id) not null,
+  favorited_date timestamp not null
+);
+CREATE INDEX idx_favorites_map_id ON favorites (map_id);
+CREATE INDEX idx_favorites_user_id ON favorites (user_id);
+/* It may be good to add a trigger-based cache for the favorite counts in the future, but for now we can accept just counting manually each time. */
