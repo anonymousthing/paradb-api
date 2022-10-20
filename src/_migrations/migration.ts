55364a78ed254300ad9766a2b35892bcae758bd3
D <me@kumo.dev>
2022-10-21 08:54:27 +1100

    migrations: fix migration setup

--- src/_migrations/migration.ts
+++ src/_migrations/migration.ts
@@ -1,12 +1,9 @@
 import { initPool } from 'db/pool';
-import { getEnvVars } from 'env';
 import path from 'path';
 
 export const setupMigration = async () => {
   require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
-  const envVars = getEnvVars();
-  await initPool(envVars);
-  return envVars;
+  await initPool();
 };
 
 // TODO: setup a test DB framework for testing migrations over fake data
